ccfe6feb28f7261cf4a55cbb9d7746ddd85c619e
Марина <mika-klubnika@yandex.ru>
2021-08-20 15:29:17 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,48 +1,52 @@
+//ФОРМА
+
+const userName = document.querySelector('input[name="userName"]');
+const userPhone = document.querySelector('input[name="tel"]');
+
+if (userName && userPhone) {
+  const getCorrectName = () => {
+    return (evt) => {
+      const valueName = userName.value;
+      const check = /^[a-zA-Zа-яА-Я]*$/;
+
+      for (let i = 0; i < valueName.length; i++) {
+        !check.test(valueName[i]) ? userName.setCustomValidity('Имя должно состоять только букв') : userName.setCustomValidity('');
+      }
+    }
+  };
+
+  userName.addEventListener('input', getCorrectName());
+
+  const getCorrectPhone = () => {
+    return (evt) => {
+      const valuePhone = userPhone.value;
+      const check = /^[0-9]*$/;
+
+      for (let i = 0; i < valuePhone.length; i++) {
+        !check.test(valuePhone[i]) ? userPhone.setCustomValidity('Номер телефона должен состоять из цифр') : userPhone.setCustomValidity('');
+      }
+    }
+  }
+
+  userPhone.addEventListener('input', getCorrectPhone());
+}
+
 //МЕНЮ
 
 const menu = document.querySelector('.navigation');
 const button = document.querySelector('.navigation__toggle');
 
-menu.classList.remove('navigation--nojs');
+if (menu && button) {
+  menu.classList.remove('navigation--nojs');
 
-button.addEventListener('click', () => {
-  menu.classList.toggle("navigation--close")
-  menu.classList.toggle("navigation--open")
+  button.addEventListener('click', () => {
+    menu.classList.toggle("navigation--close")
+    menu.classList.toggle("navigation--open")
 
-  if (menu.classList.contains("navigation--close")) {
-    button.setAttribute("aria-label", "Открыть меню");
-  } else {
-    button.setAttribute("aria-label", "Закрыть меню");
-  }
-});
-
-//ФОРМА
-const form = document.querySelector('.form');
-const userName = form.querySelector('input[name="userName"]');
-const userPhone = form.querySelector('input[name="tel"]');
-
-const getCorrectName = () => {
-  return (evt) => {
-    const valueName = userName.value;
-    const check = /^[a-zA-Zа-яА-Я]*$/;
-
-    for (let i = 0; i < valueName.length; i++) {
-      !check.test(valueName[i]) ? userName.setCustomValidity('Имя должно состоять только букв') : userName.setCustomValidity('');
+    if (menu.classList.contains("navigation--close")) {
+      button.setAttribute("aria-label", "Открыть меню");
+    } else {
+      button.setAttribute("aria-label", "Закрыть меню");
     }
-  }
-};
-
-userName.addEventListener('input', getCorrectName());
-
-const getCorrectPhone = () => {
-  return (evt) => {
-    const valuePhone = userPhone.value;
-    const check = /^[0-9]*$/;
-
-    for (let i = 0; i < valuePhone.length; i++) {
-      !check.test(valuePhone[i]) ? userPhone.setCustomValidity('Номер телефона должен состоять из цифр') : userPhone.setCustomValidity('');
-    }
-  }
+  });
 }
-
-userPhone.addEventListener('input', getCorrectPhone());
